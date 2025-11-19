bc66b1346373c6b2c6afda28d48c0871ce546780
dcarabadjac <dcarabadjac@llr.in2p3.fr>
2025-11-19 23:24:41 +0200

    Implemented the solution for PS3-4 for unsupervised case

--- problem-sets/PS3/src/p04_gmm.py
+++ problem-sets/PS3/src/p04_gmm.py
@@ -1,51 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+from scipy.stats import multivariate_normal
 
 PLOT_COLORS = ['red', 'green', 'blue', 'orange']  # Colors for your plots
 K = 4           # Number of Gaussians in the mixture model
 NUM_TRIALS = 3  # Number of trials to run (can be adjusted for debugging)
 UNLABELED = -1  # Cluster label for unlabeled data points (do not change)
-
-
-def main(is_semi_supervised, trial_num):
-    """Problem 3: EM for Gaussian Mixture Models (unsupervised and semi-supervised)"""
-    print('Running {} EM algorithm...'
-          .format('semi-supervised' if is_semi_supervised else 'unsupervised'))
-
-    # Load dataset
-    train_path = os.path.join('..', 'data', 'ds3_train.csv')
-    x, z = load_gmm_dataset(train_path)
-    x_tilde = None
-
-    if is_semi_supervised:
-        # Split into labeled and unlabeled examples
-        labeled_idxs = (z != UNLABELED).squeeze()
-        x_tilde = x[labeled_idxs, :]   # Labeled examples
-        z = z[labeled_idxs, :]         # Corresponding labels
-        x = x[~labeled_idxs, :]        # Unlabeled examples
-
-    # *** START CODE HERE ***
-    # (1) Initialize mu and sigma by splitting the m data points uniformly at random
-    # into K groups, then calculating the sample mean and covariance for each group
-    # (2) Initialize phi to place equal probability on each Gaussian
-    # phi should be a numpy array of shape (K,)
-    # (3) Initialize the w values to place equal probability on each Gaussian
-    # w should be a numpy array of shape (m, K)
-    # *** END CODE HERE ***
-
-    if is_semi_supervised:
-        w = run_semi_supervised_em(x, x_tilde, z, w, phi, mu, sigma)
-    else:
-        w = run_em(x, w, phi, mu, sigma)
-
-    # Plot your predictions
-    z_pred = np.zeros(m)
-    if w is not None:  # Just a placeholder for the starter code
-        for i in range(m):
-            z_pred[i] = np.argmax(w[i])
-
-    plot_gmm_preds(x, z_pred, is_semi_supervised, plot_id=trial_num)
 
 
 def run_em(x, w, phi, mu, sigma):
@@ -73,16 +34,28 @@ def run_em(x, w, phi, mu, sigma):
     # See below for explanation of the convergence criterion
     it = 0
     ll = prev_ll = None
+    m, k = w.shape
+    nom = np.zeros((m, k))
+    
     while it < max_iter and (prev_ll is None or np.abs(ll - prev_ll) >= eps):
-        pass  # Just a placeholder for the starter code
-        # *** START CODE HERE
-        # (1) E-step: Update your estimates in w
-        # (2) M-step: Update the model parameters phi, mu, and sigma
-        # (3) Compute the log-likelihood of the data to check for convergence.
-        # By log-likelihood, we mean `ll = sum_x[log(sum_z[p(x|z) * p(z)])]`.
-        # We define convergence by the first iteration where abs(ll - prev_ll) < eps.
-        # Hint: For debugging, recall part (a). We showed that ll should be monotonically increasing.
-        # *** END CODE HERE ***
+        for j, (muj, sigmaj, phij) in enumerate(zip(mu, sigma, phi)):
+            rv = multivariate_normal(mean=muj, cov=sigmaj)
+            px_for_zj = rv.pdf(x)
+            pz = phij
+            nom[:, j] = px_for_zj*pz
+        denom = np.sum(nom, axis=1, keepdims=True) #p(x)
+        w = nom/denom #shape m, k
+
+        sumw = np.sum(w, axis=0, keepdims=True).T
+        phi = sumw/m
+        mu = w.T@x/sumw
+
+        sigma = np.zeros_like(sigma)
+        for j in range(k):
+            sigma[j] = w.T[j].reshape(1, -1)*(x - mu[j]).T@(x - mu[j])/ sumw[j]
+
+        prev_ll = ll
+        ll = np.sum(np.log(denom))
 
     return w
 
